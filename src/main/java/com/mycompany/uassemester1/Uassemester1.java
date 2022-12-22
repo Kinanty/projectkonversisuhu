deaa9705839390d8b45d33b9621a11815a1ae1ee
Kinanty <kinantykusuma@gmail.com>
2022-12-22 11:57:08 +0700

    base project

---
+++ src/main/java/com/mycompany/uassemester1/Uassemester1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.uassemester1;
+
+/**
+ *
+ * @author MY COMPUTER
+ */
+public class Uassemester1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
